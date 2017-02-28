a443a41f90942400a77fe87e550817ea3066423f
Ankur Ankan <ankurankan@gmail.com>
2017-02-28 08:49:44 +0100

    GP classifier

---
+++ matlab/classifiers/GP.m
@@ -0,0 +1,37 @@
+classdef GP
+    properties
+        train_x
+        train_y
+    end
+    
+    methods
+        function K = rbf(obj, x, y)
+            n = size(x, 1);
+            euclidean_dist = zeros(n, n);
+            for i=1:n
+                for j=1:n
+                    euclidean_dist(i, j) = sum((x(i, :) - y(j, :)).^2);
+                end;
+            end;
+    
+            gamma = 1 / n;
+            K = exp(-gamma * euclidean_dist);
+        end
+        
+        function [mu_, s_] = predict(obj, x)
+            K_ = obj.rbf(obj.train_x, obj.train_x);
+            K_x = obj.rbf(obj.train_x, x);
+            K_xx = obj.rbf(x, x);
+            
+            m_inv = inv(K_);
+            mu_ = K_x' * m_inv * obj.train_y;
+            s_ = K_xx - K_x' * m_inv * K_x;
+        end
+        
+        function = update(obj, x, y)
+            n = size(train_x, 1);
+            train_x(n+1, :) = x;
+            train_y(n+1, :) = y;
+        end
+    end
+end
